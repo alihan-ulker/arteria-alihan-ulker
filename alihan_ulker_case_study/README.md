bc3b3b5038725ced582a85765b3977e991c58777
Alihan ÜLKER <ali.ulker.217@gmail.com>
2024-02-12 21:56:05 +0300

    initial commit

---
+++ alihan_ulker_case_study/README.md
@@ -0,0 +1,3 @@
+# alihan_ulker_case_study
+
+A new Flutter project.
---
+++ alihan_ulker_case_study/android/app/src/main/kotlin/com/example/alihan_ulker_case_study/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.alihan_ulker_case_study
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
